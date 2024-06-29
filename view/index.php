a70e25d907b3db5aa7974e4a196d4f189336928e
wo0z1e <eraz5045@gmail.com>
2024-06-29 21:21:35 +0600

    home page done

--- view/index.php
+++ view/index.php
@@ -372,7 +372,50 @@
 
     <footer class="section-p1">
         <div class="col">
-            <img src="img/logo.png" alt="">
+            <img class="logo" src="img/logo.png" alt="">
+            <h4>Contact</h4>
+            <p><strong>Address :</strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <p><strong>Phone :</strong>...................</p>
+            <p><strong>Hours :</strong>...................</p>
+            <div class="follow">
+            <h4>Follow us</h4>
+            <div class="icon">
+                <i class="fab fa-facebook-f" ></i>
+                <i class="fab fa-instagram" ></i>
+                <i class="fab fa-twitter" ></i>
+                <i class="fab fa-pinterest-p" ></i>
+                <i class="fab fa-youtube" ></i>
+            </div>
+        </div>
+        </div>
+        <div class="col">
+            <h4>About</h4>
+            <a href="#">About Us</a>
+            <a href="#">Delivery Information</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Conditions</a>
+            <a href="#">Contact Us</a>
+        </div>
+        <div class="col">
+            <h4>My Account</h4>
+            <a href="#">Sign In</a>
+            <a href="#">View Cart</a>
+            <a href="#">My Wishlist</a>
+            <a href="#">Track My Order</a>
+            <a href="#">Help</a>
+        </div>
+        <div class="col install">
+            <h4>Install App</h4>
+            <p>From App Store or Google Play</p>
+            <div class="row">
+                <img src="img/pay/app.jpg" alt="">
+                <img src="img/pay/play.jpg" alt="">
+            </div>
+            <p>Secured Payment Getways </p>
+            <img src="img/pay/pay.png" alt="">
+        </div> 
+        <div class="copyright">
+            <p>© 2021. Tech2 etc - HTML CSS Ecommerce Template</p>
         </div>
     </footer>
     <script src="../controller/script/script.js"></script>
