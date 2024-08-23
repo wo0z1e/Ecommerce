89f5fd37dee9022c25acabe4b0af0c6f75b6e61e
wo0z1e <eraz5045@gmail.com>
2024-08-23 22:53:52 +0600

    nothing

--- view/sproduct.php
+++ view/sproduct.php
@@ -24,40 +24,8 @@
         </div>
     </section>
     <section id="prodetails" class="section-p1">
-        <div class="single-pro-img">
-            <img src="img/products/f1.jpg" width="100%" id="MainImg" >
-
-            <div class="small-img-group">
-                <div class="small-img-col">
-                    <img src="img/products/f1.jpg" width="100%" class="small-img" >
-                </div>
-                <div class="small-img-col">
-                    <img src="img/products/f2.jpg" width="100%" class="small-img" alt="">
-                </div>
-                <div class="small-img-col">
-                    <img src="img/products/f3.jpg" width="100%" class="small-img" alt="">
-                </div>
-                <div class="small-img-col">
-                    <img src="img/products/f4.jpg" width="100%" class="small-img" alt="">
-                </div>
-            </div>
-        </div>
-        <div class="single-pro-details">
-            <h6>Home/T-Shirt</h6>
-            <h4>Men's Fashion T Shirt</h4>
-            <h2>$139.00</h2>
-            <select >
-                <option >Select Size</option>
-                <option >XL</option>
-                <option >XXL</option>
-                <option >Small</option>
-                <option >Large</option>
-            </select>
-            <input type="number" value="1">
-            <button class="normal">Add To Cart</button>
-            <h4>Product Details</h4>
-            <span>The Gildan Ultra Cotton T-shirt is made from a substantial 6.0 oz. per sq. yd. fabric constructed from 100% cotton, this classic fit preshrunk jersey knit provides unmatched comfort with each wear. Featuring a taped neck and shoulder, and a seamless double-needle collar, and available in a range of colors, it offers it all in the ultimate head-turning package.</span>
-        </div>
+        
+    
     </section>
     <section id="newsletter" class="section-p1 section-m1">
         <div class="newstext">
