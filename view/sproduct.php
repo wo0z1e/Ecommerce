34d22275470f6d848b64a6efeac8c007bb20d64b
wo0z1e <eraz5045@gmail.com>
2024-09-11 01:34:07 +0600

    single product page done

--- view/sproduct.php
+++ view/sproduct.php
@@ -24,8 +24,110 @@
         </div>
     </section>
     <section id="prodetails" class="section-p1">
-        
-    
+        <div class="single-pro-image">
+            <img src="img/products/f1.jpg" width="100%" id="MainImg" alt="">
+            <div class="small-img-group">
+                <div class="small-img-col" >
+                <img src="img/products/f1.jpg" width="100%" class="small-img" alt="">
+                </div>
+                <div class="small-img-col" >
+                <img src="img/products/f2.jpg" width="100%" class="small-img"  alt="">
+                </div>
+                <div class="small-img-col" >
+                <img src="img/products/f3.jpg" width="100%" class="small-img"  alt="">
+                </div>
+                <div class="small-img-col" >
+                <img src="img/products/f4.jpg" width="100%" class="small-img"  alt="">
+                </div>
+            </div>
+        </div>
+        <div class="single-pro-details">
+            <h6>Home /T-shirt</h6>
+            <h4>Men's Fashion T-Shirt</h4>
+            <h2>$139.00</h2>
+
+            <select name="" id="">
+                <option value="">Select Size</option>
+                <option value="">XL</option>
+                <option value="">XXL</option>
+                <option value="">Small</option>
+                <option value="">Large</option>
+            </select>
+            <input type="number" value="1" >
+            <button class="normal" >Add To Cart</button>
+            <h4>Product Details</h4>
+            <span>The Gildan Ultra Cotton T-shirt is made from a substantial 6.0 oz. per sq. yd. fabric constructed from 100% cotton, this classic fit preshrunk jersey knit provides unmatched comfort with each wear. Featuring a taped neck and shoulder, and a seamless double-needle collar, and available in a range of colors, it offers it all in the ultimate head- turning package.</span>
+        </div>
+    </section>
+    <section id="product1" class="section-p1">
+        <h2>Featured Products</h2>
+        <p>Summer Collection New Modern Design</p>
+        <div class="pro-container">
+            <div class="pro">
+                <img src="img/products/n1.jpg" alt="">
+                <div class="des">
+                    <span>adidas</span>
+                    <h5>Cartoon Astronaut T-Shirts</h5>
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>$78</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus cart"></i></a>
+            </div>
+            <div class="pro">
+                <img src="img/products/n2.jpg" alt="">
+                <div class="des">
+                    <span>adidas</span>
+                    <h5>Cartoon Astronaut T-Shirts</h5>
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>$78</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus cart"></i></a>
+            </div>
+            <div class="pro">
+                <img src="img/products/n3.jpg" alt="">
+                <div class="des">
+                    <span>adidas</span>
+                    <h5>Cartoon Astronaut T-Shirts</h5>
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>$78</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus cart"></i></a>
+            </div>
+            <div class="pro">
+                <img src="img/products/n4.jpg" alt="">
+                <div class="des">
+                    <span>adidas</span>
+                    <h5>Cartoon Astronaut T-Shirts</h5>
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>$78</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus cart"></i></a>
+            </div>
+        </div>
     </section>
     <section id="newsletter" class="section-p1 section-m1">
         <div class="newstext">
@@ -85,6 +187,27 @@
             <p>© 2021. Tech2 etc - HTML CSS Ecommerce Template</p>
         </div>
     </footer>
+
+    <script>
+      var MainImg = document.getElementById("MainImg") ;
+      var smallimg = document.getElementsByClassName("small-img")
+
+      smallimg[0].onclick=function(){
+        MainImg.src=smallimg[0].src;
+      }
+
+      smallimg[1].onclick=function(){
+        MainImg.src=smallimg[1].src;
+      }
+
+      smallimg[2].onclick=function(){
+        MainImg.src=smallimg[2].src;
+      }
+
+      smallimg[3].onclick=function(){
+        MainImg.src=smallimg[3].src;
+      }
+    </script>
     <script src="../controller/script/script.js"></script>
 </body>
 
